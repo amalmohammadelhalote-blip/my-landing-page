6bc8e2d9ea0bf320c9749b00e82c7741cec68e5a
amalmohammadelhalote-blip <amalmohammadelhalote@gmail.com>
2026-04-12 19:39:14 +0200

    fix build error (case sensitivity)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './pages/Login.css'
+import './pages/login.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
